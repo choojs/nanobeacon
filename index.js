8c443304e38c4de02a6daed6464aa82048d22537
Joe Hand <joe@joeahand.com>
2017-08-31 16:22:30 -0700

    Force text content-type

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function nanobeacon (url, data) {
 
   assert.ok(json.length < MAX_SIZE, 'nanobeacon: data should be smaller than ' + MAX_SIZE + ' bytes. Was ' + json.length + ' bytes')
 
-  var blob = new window.Blob([ json ])
+  var blob = new window.Blob([ json ], { type: 'text/plain; charset=UTF-8' })
   return window.navigator.sendBeacon(url, blob)
 }
